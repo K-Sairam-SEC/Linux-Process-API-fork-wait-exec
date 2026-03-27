8999132d085b22f57178550c899f2cb00ed9070c
K-Sairam-SEC <phexljs@gmail.com>
2026-03-27 17:23:46 +0000

    moving file into repo

--- execcheck2.c
+++ execcheck2.c
@@ -1,1 +1,25 @@
-//C Program to execute Linux system commands using Linux API system calls exec() family
+#include <stdio.h>
+#include <stdlib.h>
+#include <unistd.h>
+#include <sys/wait.h>
+#include <stddef.h>
+
+int main() {
+    pid_t pid = fork();
+
+    if (pid < 0) {
+        perror("fork failed");
+        return 1;
+    }
+
+    if (pid == 0) { 
+        printf("I am child, my PID is %d\n", getpid()); 
+        printf("My parent PID is: %d\n", getppid()); 
+        sleep(2); 
+    } else { 
+        printf("I am parent, my PID is %d\n", getpid()); 
+        wait(NULL); 
+        printf("Child has finished. Parent exiting.\n");
+    }
+    return 0;
+}
